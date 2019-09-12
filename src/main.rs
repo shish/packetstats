58e5bce3e6cb8b03aaba8aaaf6a5b138217f817c
Shish <shish@shishnet.org>
2019-09-12 02:15:18 +0100

    format

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate etherparse;
 extern crate pcap;
 use etherparse::*;
 
-use argparse::{ArgumentParser, Store, StoreOption};  // , StoreTrue};
+use argparse::{ArgumentParser, Store, StoreOption}; // , StoreTrue};
 use dns_lookup::lookup_addr;
 use pcap::{Capture, Device};
 use std::collections::HashMap;
@@ -41,15 +41,11 @@ fn main() {
     run_capture(&device, &mut cap, &socket);
 }
 
-fn run_capture(
-    device: &String,
-    cap: &mut pcap::Capture<pcap::Active>,
-    socket: &Option<String>,
-) {
+fn run_capture(device: &String, cap: &mut pcap::Capture<pcap::Active>, socket: &Option<String>) {
     let mut resolv: HashMap<IpAddr, String> = HashMap::new();
     let mut hosts: HashMap<String, u64> = HashMap::new();
     let mut now = Instant::now();
-	let freq = 10;
+    let freq = 10;
     let my_mac = [0xDC, 0xA6, 0x32, 0x09, 0xA8, 0x88]; // /sys/class/net/eth0/address
 
     while let Ok(packet) = cap.next() {
@@ -93,10 +89,10 @@ fn run_capture(
                 };
 
                 // Get a connection name
-				if !resolv.contains_key(&remote_ip) {
-					resolv.insert(remote_ip, lookup_addr(&remote_ip).unwrap());
-				}
-				let remote_name = resolv.get(&remote_ip).unwrap();
+                if !resolv.contains_key(&remote_ip) {
+                    resolv.insert(remote_ip, lookup_addr(&remote_ip).unwrap());
+                }
+                let remote_name = resolv.get(&remote_ip).unwrap();
                 let (proto, port) = match value.transport {
                     Some(Udp(value)) => (
                         "udp",
@@ -117,31 +113,35 @@ fn run_capture(
                     None => continue,
                 };
 
-				let dir = if out {"send"} else {"recv"};
+                let dir = if out { "send" } else { "recv" };
 
-                let connection = format!("address={},counter={},protocol={},port={}", remote_name, dir, proto, port).to_string();
+                let connection = format!(
+                    "address={},counter={},protocol={},port={}",
+                    remote_name, dir, proto, port
+                )
+                .to_string();
 
                 // increment sent or received hashmap
-				*hosts.entry(connection).or_insert(0) += packet_len;
+                *hosts.entry(connection).or_insert(0) += packet_len;
 
                 if now.elapsed().as_secs() > freq {
                     now = Instant::now();
-					for (key, value) in &hosts {
-						let line = format!(
-							"packetstats,interface={},{} value={}",
-							device,
-							key,
-							value / freq
-						);
-						match socket {
-							Some(socket) => {
-								let s = UnixDatagram::unbound().unwrap();
-								s.send_to(line.as_bytes(), socket).unwrap();
-							},
-							None => println!("{}", line)
-						}
-					}
-					hosts.clear()
+                    for (key, value) in &hosts {
+                        let line = format!(
+                            "packetstats,interface={},{} value={}",
+                            device,
+                            key,
+                            value / freq
+                        );
+                        match socket {
+                            Some(socket) => {
+                                let s = UnixDatagram::unbound().unwrap();
+                                s.send_to(line.as_bytes(), socket).unwrap();
+                            }
+                            None => println!("{}", line),
+                        }
+                    }
+                    hosts.clear()
                 }
             }
         }
